5bfc8293ede73d8481e7498c906099477c23f938
Mandip Amgain <mandip.amgain123@gmail.com>
2023-05-21 04:30:34 -0400

    Envirnment Setup

---
+++ src/main/java/OpenSource/EcoExchange/EcoExchangeApplication.java
@@ -0,0 +1,13 @@
+package OpenSource.EcoExchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoExchangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/OpenSource/EcoExchange/EcoExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package OpenSource.EcoExchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
